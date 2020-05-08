27fc948966f7ee1c3d3d8e1404457b6beffad59f
WhiteShadow13 <ludo.m@live.fr>
2020-05-08 18:07:26 +0200

    fix logic

--- maxone.py
+++ maxone.py
@@ -1,6 +1,7 @@
 # maxone.py
 # Author: Sébastien Combéfis
 # Version: April 26, 2020
+# Modified by Ludovic Merel
 
 import random
 import matplotlib.pyplot as plt
@@ -11,14 +12,11 @@ from tabulate import tabulate
 PROB_MATING = 0.5
 PROB_MUTATION = 0.2
 
-# Initialising the population.
-population = []
-
 # Defining the fitness function.
 def evaluate(ind):
     return sum(ind)
 
-# Defining the mating function.
+# Defining the mating functions.
 def uniform_mate(ind1, ind2):
     assert len(ind1) == len(ind2), "ind1 & ind2 should be the same length"
     child1 = ind1.copy()
@@ -32,15 +30,10 @@ def uniform_mate(ind1, ind2):
 
 def cross_mate(ind1, ind2):
     assert len(ind1) == len(ind2), "ind1 & ind2 should be the same length"
-    child1 = ind1.copy()
-    child2 = ind2.copy()
 
     pivot = int(round(len(ind1)/2, 0))
 
-    child1 = ind1[:pivot] + ind2[pivot:]
-    child2 = ind1[pivot:] + ind2[:pivot]
-
-    return child1, child2
+    return ind1[:pivot] + ind2[pivot:], ind1[pivot:] + ind2[:pivot]
 
 # Defining the mutation function.
 def mutate(ind):
@@ -75,17 +68,11 @@ def select(pop):
     return tuple(parents)
 
 # GA
-def main(pop_size: int, iterations: int, mate_function):
-    IND_SIZE = 10
-    POP_SIZE = pop_size
-    ITERATIONS = iterations
-    
-    # Create initial population
-    for i in range(POP_SIZE):
-        population.append([random.randrange(2) for i in range(IND_SIZE)])
+def genetic_algorithm(pop: list, iterations: int, mate_function):
+    population = pop.copy()
 
     stats = list()
-    for i in range(ITERATIONS):
+    for i in range(iterations):
         parents = select(population)
         children = mate_function(parents[0], parents[1])
         mutate_children = list(map(mutate, children))
@@ -96,17 +83,17 @@ def main(pop_size: int, iterations: int, mate_function):
             del population[iteration_fitness.index(min(iteration_fitness))]
 
         # Add children to population
-        for child in mutate_children:
-            population.append(child)
+        population.extend(mutate_children)
 
         iteration_fitness = np.array(list(map(evaluate, population)))
-        stats.append((i+1, np.mean(iteration_fitness), np.min(iteration_fitness), np.max(iteration_fitness)))
+        stats.append((i+1, np.mean(iteration_fitness)))
     
     return stats
 
 # Running the simulation.
 if __name__ == '__main__':
-    ITERATIONS = 150
+    ITERATIONS = 300
+    IND_SIZE = 10
     
     # Parameters to change
     pop_size_list = [20, 50, 100, 150, 200]
@@ -114,24 +101,26 @@ if __name__ == '__main__':
 
     # Utils for plot
     mate_function_subplot_list= [211, 212]
-    plot_title_list = ["Mean value by iteration for uniform mate", "Mean value by iteration for cross mate"]
+    plot_title_list = ['Mean value by iteration for uniform mate', 'Mean value by iteration for cross mate']
     linestyle_list = ['dashed', 'dashdot', 'dotted']
 
     stats_dict = dict()
     for pop_size in pop_size_list:
         if pop_size not in stats_dict:
             stats_dict[pop_size] = dict()
+        
+        initial_population = [[random.randrange(2) for i in range(IND_SIZE)] for i in range(pop_size)]
 
         for mate_function in mate_function_list:
             if mate_function.__name__ not in stats_dict[pop_size]:
                 stats_dict[pop_size][mate_function.__name__] = list()
 
-            stats = main(pop_size, ITERATIONS, mate_function)
-            stats_dict[pop_size][mate_function.__name__].extend(stats)
+            stats_dict[pop_size][mate_function.__name__].extend(genetic_algorithm(initial_population, ITERATIONS, mate_function))
 
     
     # Display plots
     df = pd.DataFrame.from_dict(stats_dict)
+    print(df)
 
     for pop_index, pop_size in enumerate(pop_size_list):
         for mate_index, mate_function in enumerate(mate_function_list):
@@ -140,7 +129,8 @@ if __name__ == '__main__':
                 np.arange(ITERATIONS), 
                 [stat[1] for stat in df[pop_size][mate_function.__name__]], 
                 label='{}'.format(pop_size),
-                linestyle=linestyle_list[pop_index%len(linestyle_list)])
+                linestyle=linestyle_list[pop_index%len(linestyle_list)]
+                )
     
             ax.legend()
 
